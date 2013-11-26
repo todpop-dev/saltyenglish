8c84469a097fa74ff75b1aa400a4f98fb2cfc10c
honeyl3oom <ddubool92@gmail.com>
2013-11-27 04:25:42 +0900

    StudyTestFinish // add set_cpdm_log

--- SaltyEnglish/src/com/todpop/saltyenglish/StudyTestFinish.java
+++ SaltyEnglish/src/com/todpop/saltyenglish/StudyTestFinish.java
@@ -6,6 +6,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -24,7 +26,11 @@ public class StudyTestFinish extends Activity {
 	
 	Button skipBtn;	
 	SharedPreferences rgInfo;
-
+	private int video_length =0;
+	private VideoView video;
+	private int ad_id = -1;
+	private int ad_type = 201;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,6 +38,7 @@ public class StudyTestFinish extends Activity {
 		setContentView(R.layout.activity_study_test_finish);
 		skipBtn = (Button)findViewById(R.id.testfinish_id_skip_btn);
 		rgInfo = getSharedPreferences("rgInfo",0);
+		video = (VideoView)findViewById(R.id.test_video_view);
 		new GetCPDM().execute("http://todpop.co.kr/api/advertises/get_cpdm_ad.json?user_id="+rgInfo.getString("mem_id", "0"));
 
 		
@@ -51,64 +58,119 @@ public class StudyTestFinish extends Activity {
 	}
 
 
-		private Runnable mLaunchTaskMain = new Runnable() {
-			public void run() {
-				skipBtn.setEnabled(true);
-				skipBtn.setBackgroundResource(R.drawable.studytestfinish_drawable_btn_skip);
-			}
-		};
+	private Runnable mLaunchTaskMain = new Runnable() {
+		public void run() {
+			skipBtn.setEnabled(true);
+			skipBtn.setBackgroundResource(R.drawable.studytestfinish_drawable_btn_skip);
+		}
+	};
 
 	
 	
 	//--- request class ---
-		private class GetCPDM extends AsyncTask<String, Void, JSONObject> 
+	private class GetCPDM extends AsyncTask<String, Void, JSONObject> 
+	{
+		@Override
+		protected JSONObject doInBackground(String... urls) 
 		{
-			@Override
-			protected JSONObject doInBackground(String... urls) 
-			{
-				JSONObject result = null;
-				try {
-					DefaultHttpClient httpClient = new DefaultHttpClient();
-					String getURL = urls[0];
-					HttpGet httpGet = new HttpGet(getURL);
-					HttpResponse httpResponse = httpClient.execute(httpGet);
-					HttpEntity resEntity = httpResponse.getEntity();
+			JSONObject result = null;
+			try {
+				DefaultHttpClient httpClient = new DefaultHttpClient();
+				String getURL = urls[0];
+				HttpGet httpGet = new HttpGet(getURL);
+				HttpResponse httpResponse = httpClient.execute(httpGet);
+				HttpEntity resEntity = httpResponse.getEntity();
 
-					if (resEntity != null) {    
-						result = new JSONObject(EntityUtils.toString(resEntity)); 
-						Log.d("CPDM RESPONSE ---- ", result.toString());				        	
-					}
-					return result;
-				}
-				catch (Exception e) {
-					e.printStackTrace();
+				if (resEntity != null) {    
+					result = new JSONObject(EntityUtils.toString(resEntity)); 
+					Log.d("CPDM RESPONSE ---- ", result.toString());				        	
 				}
 				return result;
 			}
-
-			@Override
-			protected void onPostExecute(JSONObject json) {
-
-				try {
-					if(json.getBoolean("status")==true) {
-						VideoView video = (VideoView)findViewById(R.id.test_video_view);
-						video.setVideoPath("http://todpop.co.kr/"+json.getJSONObject("data").getString("url"));
-						//final MediaController mc = new MediaController(LvTestFinish.this);
-						//mc.hide();
-						//video.setMediaController(mc);
-						video.start();
-						
-						Handler mHandler = new Handler();
-						mHandler.postDelayed(mLaunchTaskMain, 8000);	// should be 5000 but timing difficulty
-						
-					} else {		        
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-
+			catch (Exception e) {
+				e.printStackTrace();
 			}
+			return result;
 		}
+
+		@Override
+		protected void onPostExecute(JSONObject json) {
+
+			try {
+				if(json.getBoolean("status")==true) {
+					video_length = Integer.parseInt(json.getJSONObject("data").getString("length"));
+					ad_id = json.getJSONObject("data").getInt("ad_id");
+					video.setVideoPath("http://todpop.co.kr/"+json.getJSONObject("data").getString("url"));
+					video.setOnCompletionListener(cl);
+					video.start();
+					
+					
+					Handler mHandler = new Handler();
+					mHandler.postDelayed(mLaunchTaskMain, 8000);	// should be 5000 but timing difficulty
+					
+				} else {		        
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+	private class SetCPDMlog extends AsyncTask<String, Void, JSONObject> {
+
+		@Override
+		protected JSONObject doInBackground(String... urls) {
+			JSONObject result = null;
+			try {
+				DefaultHttpClient httpClient = new DefaultHttpClient();
+				String getURL = urls[0];
+				HttpGet httpGet = new HttpGet(getURL);
+				HttpResponse httpResponse = httpClient.execute(httpGet);
+				HttpEntity resEntity = httpResponse.getEntity();
+
+				if (resEntity != null) {    
+					result = new JSONObject(EntityUtils.toString(resEntity)); 
+					Log.d("SET CPDM LOG RESPONSE ---- ", result.toString());				        	
+				}
+				return result;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return result;
+		}
+		
+		@Override
+		protected void onPostExecute(JSONObject json) {
+
+			try {
+				if(json.getBoolean("status")==true) {
+					Intent intent = new Intent(getApplicationContext(), StudyTestResult.class);
+					startActivity(intent);
+					StudyTestFinish.this.finish();
+				} else {		        
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+		
+	}
+	
+	
+	private MediaPlayer.OnCompletionListener cl = new MediaPlayer.OnCompletionListener() {
+		
+		@Override
+		public void onCompletion(MediaPlayer mp) {
+			skipBtn.setEnabled(false);
+			Log.d("cpdm view_time----", String.valueOf(video_length));
+			new SetCPDMlog().execute("http://todpop.co.kr/api/advertises/set_cpdm_log.json?ad_id="+ad_id+"&ad_type="+ad_type+"&user_id="+rgInfo.getString("mem_id", "0")+"&view_time="+video_length);
+			Intent intent = new Intent(getApplicationContext(), StudyTestResult.class);
+			startActivity(intent);
+			finish();
+		}
+	};
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -120,8 +182,9 @@ public class StudyTestFinish extends Activity {
 	public void showTestFinishViewCB(View v)
 	{
 		v.setEnabled(false);
-		
-		
+		int view_time = (int)Math.floor(video.getCurrentPosition()/1000);
+		Log.d("cpdm view_time----",""+view_time);
+		new SetCPDMlog().execute("http://todpop.co.kr/api/advertises/set_cpdm_log.json?ad_id="+ad_id+"&ad_type="+ad_type+"&user_id="+rgInfo.getString("mem_id", "0")+"&view_time="+view_time);
 //		SharedPreferences sp = getSharedPreferences("StudyLevelInfo", 0);
 //		SharedPreferences.Editor editor = sp.edit();
 //		int totalStage = sp.getInt("totalStage", 1);
@@ -157,11 +220,6 @@ public class StudyTestFinish extends Activity {
 //			editor2.commit();
 //		}
 		
-
-		
-		Intent intent = new Intent(getApplicationContext(), StudyTestResult.class);
-		startActivity(intent);
-		finish();
 	}
 
 }
