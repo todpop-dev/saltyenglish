19199e61499cc53f44cd46daa31efea0c9fafc23
jangjunho <jangjunho7@gmail.com>
2014-06-27 17:33:34 +0900

    내 사진 저장,설정 하기 구현,

--- SaltyEnglish/src/com/todpop/saltyenglish/HomeMoreSetting.java
+++ SaltyEnglish/src/com/todpop/saltyenglish/HomeMoreSetting.java
@@ -1,22 +1,34 @@
 package com.todpop.saltyenglish;
 
+import android.annotation.SuppressLint;
+import android.app.DownloadManager.Request;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.todpop.api.FileManager;
 import com.todpop.api.TypefaceActivity;
 
-public class HomeMoreSetting extends TypefaceActivity {
+@SuppressLint("NewApi") public class HomeMoreSetting extends TypefaceActivity {
 
+	private static final int REQUEST_CODE_IMAGE = 0;
 	SharedPreferences setting;
 	SharedPreferences.Editor settingEdit;
 
@@ -46,11 +58,15 @@ public class HomeMoreSetting extends TypefaceActivity {
 	private Switch swPopupOnOff;
 	private boolean isAlarmEnabled;
 	private LinearLayout llSettingAlarm;
+	private ImageView ivMyPicture;
+	private Bitmap bmpMyPicture;
+	private FileManager fm;
 
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_more_setting);
+
 
 		setting = getSharedPreferences("setting", 0);
 		settingEdit = setting.edit();
@@ -62,8 +78,10 @@ public class HomeMoreSetting extends TypefaceActivity {
 		isPopupEnabled = setting.getBoolean("isPopupEnabled", true);
 		isAlarmEnabled = setting.getBoolean("isAlarmEnabled", true);
 		alarmTime = setting.getString("alarmTime","00:00");
-
+		
 		llSettingAlarm = (LinearLayout)findViewById(R.id.ll_setting_alarm); 
+		
+		ivMyPicture = (ImageView)findViewById(R.id.iv_setting_mypicture);
 
 		etNickName = (EditText)findViewById(R.id.et_setting_name);
 		tvFrontTime = (TextView)findViewById(R.id.tv_setting_alarm_time_front);
@@ -71,6 +89,12 @@ public class HomeMoreSetting extends TypefaceActivity {
 
 		swAlarmOnOff = (Switch)findViewById(R.id.sw_setting_alarm_onoff);
 		swPopupOnOff= (Switch)findViewById(R.id.sw_setting_popup_onoff);
+		
+		fm = new FileManager();
+		bmpMyPicture = fm.getImgFile("myPicture");
+		if(bmpMyPicture != null){
+			ivMyPicture.setBackground(new BitmapDrawable(bmpMyPicture));
+		}
 
 		swAlarmOnOff.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			@Override
@@ -110,7 +134,28 @@ public class HomeMoreSetting extends TypefaceActivity {
 	}
 
 	public void setMyPicture(View v){
-
+		Intent intent =new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		this.startActivityForResult(intent, REQUEST_CODE_IMAGE);
+	}
+	
+	@SuppressLint("NewApi") @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode == REQUEST_CODE_IMAGE && resultCode == RESULT_OK && data != null){
+			Uri selectImageUri = data.getData();
+			String[] filePathCol = {MediaStore.Images.Media.DATA};
+			
+			Cursor cursor = this.getContentResolver().query(selectImageUri, filePathCol, null, null, null);
+			cursor.moveToFirst();
+			
+			int colIndex = cursor.getColumnIndex(filePathCol[0]);
+			String imgPath = cursor.getString(colIndex);
+			cursor.close();
+			
+			bmpMyPicture = BitmapFactory.decodeFile(imgPath);
+			ivMyPicture.setBackground(new BitmapDrawable(bmpMyPicture));
+			
+		}
 	}
 
 	public void setAlarmTime(View v){
@@ -130,6 +175,9 @@ public class HomeMoreSetting extends TypefaceActivity {
 		settingEdit.putString("alarmTime", alarmTime);
 		rgInfoEdit.apply();
 		settingEdit.apply();
+		
+		fm.saveImgFile(bmpMyPicture, "myPicture");
+		
 		finish();
 	}
 
